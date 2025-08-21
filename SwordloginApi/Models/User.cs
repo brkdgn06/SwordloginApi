257a34fd2f2adc46b72c5559df32adea5cb65d2c
brkdgn06 <burak.dogan@arslanlargrup.com.tr>
2025-08-21 23:44:57 +0300

    update

--- SwordloginApi/Models/User.cs
+++ SwordloginApi/Models/User.cs
@@ -2,17 +2,8 @@
 {
     public class User
     {
-        public enum UserStatus
-        {
-            Pending,
-            Approved,
-            Rejected
-        }
-
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }      
-        public DateTime TokenExpiry { get; set; }
-
     }
 }
