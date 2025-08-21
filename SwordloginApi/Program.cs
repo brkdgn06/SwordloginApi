c4a7e36d10b22fe5d19d8c3e7158ce1c64ee182a
brkdgn06 <burak.dogan@arslanlargrup.com.tr>
2025-08-21 17:21:09 +0300

    LoginController eklendi

--- SwordloginApi/Program.cs
+++ SwordloginApi/Program.cs
@@ -12,7 +12,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectio
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls("http://0.0.0.0:10000");
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "10000"));
