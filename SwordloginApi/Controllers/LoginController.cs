12f73f5f7c0bed77d51647f5636f148fdcbb52cf
brkdgn06 <burak.dogan@arslanlargrup.com.tr>
2025-08-21 16:56:56 +0300

    LoginController eklendi

---
+++ SwordloginApi/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using SwordloginApi.Data;
+using SwordloginApi.Models;
+
+namespace SwordloginApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LoginController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] User request)
+        {
+            var user = _context.Users
+                .FirstOrDefault(u => u.Username == request.Username && u.Password == request.Password);
+
+            if (user == null)
+                return Unauthorized(new { message = "Kullanıcı adı veya şifre hatalı" });
+
+            return Ok(new { message = "Giriş başarılı", userId = user.Id });
+        }
+    }
+}
