096ad2e7e3a94d0dc6696e698835737f109e9be7
brkdgn06 <burak.dogan@arslanlargrup.com.tr>
2025-08-21 14:13:50 +0300

    Proje dosyası ekle.

---
+++ SwordloginApi/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SwordloginApi.Data;
+using SwordloginApi.Models;
+
+namespace SwordloginApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User loginData)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username == loginData.Username && u.Password == loginData.Password);
+
+            if (user == null)
+                return Unauthorized(new { message = "Geçersiz kullanıcı adı veya şifre" });
+
+            return Ok(new { message = "Giriş başarılı", userId = user.Id });
+        }
+    }
+}
---
+++ SwordloginApi/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using SwordloginApi.Data;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000); // Tüm IP'lerden gelen istekleri dinler
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.Urls.Add($"http://*:{port}");
+app.MapControllers();
+
+app.Run();
