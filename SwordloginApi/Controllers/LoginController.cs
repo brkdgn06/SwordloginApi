f56cd86e9f5bed898f28e6bf66e36c686b254e9e
brkdgn06 <burak.dogan@arslanlargrup.com.tr>
2025-08-23 01:49:46 +0300

    admin kaldırıldı

--- SwordloginApi/Controllers/LoginController.cs
+++ SwordloginApi/Controllers/LoginController.cs
@@ -20,20 +20,17 @@ namespace SwordloginApi.Controllers
         public IActionResult Login([FromBody] User request)
         {
             var user = _context.Users
+
                 .FirstOrDefault(u => u.Username == request.Username && u.Password == request.Password);
 
             if (user == null)
                 return Unauthorized(new { message = "Kullanıcı adı veya şifre hatalı" });
 
-            // Role bilgisi bit tipinde: 0 = normal, 1 = admin
-            bool isAdmin = user.Role == "admin";
-
             return Ok(new
             {
-                message = "Giriş başarılı",
-                userId = user.Id,
-                username = user.Username,
-                role = isAdmin // Unity tarafında bool olarak karşılanabilir
+                message = "Giriş başarılı", 
+                userId = user.Id, 
+                username = user.Username,   
             });
 
         }
